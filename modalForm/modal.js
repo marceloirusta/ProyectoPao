4e98f2eeb898cfe7c48079a2222803c331832d69
Marcelo <marcelo.irusta@coxautoinc.com>
2022-10-06 22:02:36 -0400

    Modal form

---
+++ modalForm/modal.js
@@ -0,0 +1,3 @@
+document.getElementById("nombreField").defaultValue = "Marcelo";
+document.getElementById("cursoField").defaultValue = "6D";
+document.getElementById("edadField").defaultValue = "16";
